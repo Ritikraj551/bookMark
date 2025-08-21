6db207a9cce49ae3817fa2a969d9bd923a99e107
Ritikraj551 <rtkrajs551@gmail.com>
2025-08-22 02:46:55 +0530

    Made the panel tabs functionality

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
-const themeButton = document.getElementById("toggleTheme");
-const rootElement = document.documentElement;
+const tabs = document.querySelectorAll(".tab");
+const panels = document.querySelectorAll(".panel");
 
-themeButton.addEventListener("click", () => {
-  if (rootElement.classList.contains("dark")) {
-    rootElement.classList.remove("dark");
-    rootElement.classList.add("light");
-  } else {
-    rootElement.classList.remove("light");
-    rootElement.classList.add("dark");
-  }
-});
+tabs.forEach((tab)=>tab.addEventListener('click',onTabClick))
+
+function onTabClick(e){
+  tabs.forEach((tab)=>{
+    tab.children[0].classList.remove(
+      "border-red-500",
+      "border-b-4",
+      "md:border-b-0"
+    )
+  })
+
+  // Hide all Panels
+  panels.forEach((panel)=>panel.classList.add("hidden"));
+
+  // Activate a new tab and panel based on the target
+  e.target.classList.add("border-red-500","border-b-4")
+  const classString = e.target.getAttribute("data-target");
+  document.getElementById('panels').getElementsByClassName(classString)[0].classList.remove("hidden");
+}
